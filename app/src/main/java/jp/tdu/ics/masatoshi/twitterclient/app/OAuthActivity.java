369f2dce66b14e1c1029123a42b9cce46469d33e
MasatoshiTanaka <maasansun3@gmail.com>
2015-03-23 03:18:30 +0900

    no message

--- app/src/main/java/jp/tdu/ics/masatoshi/twitterclient/app/OAuthActivity.java
+++ app/src/main/java/jp/tdu/ics/masatoshi/twitterclient/app/OAuthActivity.java
@@ -1,7 +1,15 @@
 package jp.tdu.ics.masatoshi.twitterclient.app;
 
+import android.app.Activity;
+import android.os.Bundle;
+
 /**
  * Created by Tanakan on 15/03/23.
  */
-public class OAuthActivity {
+public class OAuthActivity extends Activity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+    }
 }
