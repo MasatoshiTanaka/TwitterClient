addfec8340a6bfee30c179b105e25ca489abd96e
MasatoshiTanaka <maasansun3@gmail.com>
2015-03-29 00:57:35 +0900

    no message

--- app/src/main/java/jp/tdu/ics/masatoshi/twitterclient/app/TwitterActivity.java
+++ app/src/main/java/jp/tdu/ics/masatoshi/twitterclient/app/TwitterActivity.java
@@ -1,5 +1,6 @@
 package jp.tdu.ics.masatoshi.twitterclient.app;
 
+import android.app.Activity;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -7,7 +8,7 @@ import android.view.MenuItem;
 import twitter4j.Twitter;
 
 
-public class TwitterActivity extends ActionBarActivity {
+public class TwitterActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
